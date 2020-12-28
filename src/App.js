76b4a664f3c0a8963a64b4c36143403ba2933c55
Igor Zinar <igorzinar@gmail.com>
2020-12-28 19:29:54 +0200

    install react-infinity-scroll, add in jsx infinity scroll and add to useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import './App.css'
 import axios from 'axios'
 
@@ -6,22 +7,30 @@ const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY
 
 export default function App() {
   const [images, setImages] = useState([])
+  const [page, setPage] = useState(1)
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await axios(
-          `https://api.unsplash.com/photos/?client_id=${accessKey}`
-        )
-        const data = result.data
-        setImages(data)
-      } catch (error) {
-        console.error(error)
-      }
-    }
-    fetchData()
-  }, [])
+    getPhoto()
+  }, [page])
 
+  function getPhoto() {
+    fetch(
+      `https://api.unsplash.com/photos/?client_id=${accessKey}&page=${page}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages((images) => [...images, ...data])
+      })
+  }
+
+  // Try to rewrite useState to add next page
+  const fetchData = async () => {
+    const result = await axios(
+      `https://api.unsplash.com/photos/?client_id=${accessKey}`
+    )
+    const data = result.data
+    setImages(data)
+  }
   // return an error if there is no access key
   if (!accessKey) {
     return (
@@ -39,14 +48,26 @@ export default function App() {
         <input type="text" placeholder="Search Image..." />
         <button>Search</button>
       </form>
-
-      <div className="image-grid">
-        {images.map((image, index) => (
-          <div className="image" key={index}>
-            <img src={image.urls.regular} alt={image.alt_description} />
-          </div>
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={images.length} //This is important field to render the next data
+        next={() => setPage((page) => page + 1)}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className="image-grid">
+          {images.map((image, index) => (
+            <a
+              className="image"
+              key={index}
+              href={image.links.html}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={image.urls.regular} alt={image.alt_description} />
+            </a>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   )
 }
