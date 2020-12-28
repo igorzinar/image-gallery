be881386a286d821715d72c1befb6eb7e943cbb9
Igor Zinar <igorzinar@gmail.com>
2020-12-28 14:18:19 +0200

    add accessKey to fetch data and put data in useState and put it in src of images list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY
+
 export default function App() {
+  const [images, setImages] = useState([])
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const result = await axios(
-          'https://api.unsplash.com/photos/?client_id=lkYpZkmURFesR8bn_odqbt1p_qH060LI0XeI4w8Vqj4'
+          `https://api.unsplash.com/photos/?client_id=${accessKey}`
         )
-        console.log(result.data)
-        // const data = await result.json()
-        // console.log(data)
+        const data = result.data
+        setImages(data)
       } catch (error) {
-        console.error("can't find image")
+        console.error(error)
       }
     }
     fetchData()
   }, [])
+
+  // return an error if there is no access key
+  if (!accessKey) {
+    return (
+      <a href="https://unsplash.com/developers" className="error">
+        Required: Get Your Unsplash API Key
+      </a>
+    )
+  }
+
   return (
     <div className="app">
       <h1>Infinity Image Gallery!</h1>
@@ -28,9 +41,9 @@ export default function App() {
       </form>
 
       <div className="image-grid">
-        {[...Array(100)].map((_, index) => (
+        {images.map((image, index) => (
           <div className="image" key={index}>
-            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
+            <img src={image.urls.regular} alt={image.alt_description} />
           </div>
         ))}
       </div>
