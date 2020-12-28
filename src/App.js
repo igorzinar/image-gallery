54e7c5368b63a7f484fe9608e1ad6636ca54e5ff
Igor Zinar <igorzinar@gmail.com>
2020-12-28 06:58:59 +0200

    add fetcging data from unsplash.com with async await in eseEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
 
 export default function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios(
+          'https://api.unsplash.com/photos/?client_id=lkYpZkmURFesR8bn_odqbt1p_qH060LI0XeI4w8Vqj4'
+        )
+        console.log(result.data)
+        // const data = await result.json()
+        // console.log(data)
+      } catch (error) {
+        console.error("can't find image")
+      }
+    }
+    fetchData()
+  }, [])
   return (
     <div className="app">
       <h1>Infinity Image Gallery!</h1>
